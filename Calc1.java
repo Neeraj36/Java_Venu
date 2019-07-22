8962ffb9adc42f035cbfb3465aee0ca53923875d
Neeraj36 <53039469+Neeraj36@users.noreply.github.com>
2019-07-22 20:43:38 +0530

    supporting file for Calc program
    Calc1 is supporting method  file for Calc progra.

---
+++ Calc1.java
@@ -0,0 +1,23 @@
+
+public class Calc1 {
+	public int add (int x , int y ) {
+		int z;
+		z=x+y;
+		return z;
+	}
+	public int sub (int x , int y ) {
+		int z;
+		z=x-y;
+		return z;
+	}
+	public int multi (int x , int y ) {
+		int z;
+		z=x*y;
+		return z;
+	}
+	public int dev (int x , int y ) {
+		int z;
+		z=x/y;
+		return z;
+	}
+}
