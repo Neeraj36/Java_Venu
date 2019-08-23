6ab04c9a26b2d2672a9bd87339421c36a0e83942
Neeraj36 <53039469+Neeraj36@users.noreply.github.com>
2019-08-23 11:06:32 +0530

    Assignment-5

---
+++ anagram.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Solution {
+ static boolean isAnagram(String a, String b) {
+        
+        String[] arr1 = a.trim().split("");
+
+        String[] arr2 = b.trim().split("");
+
+        int count = 0;
+
+        if (arr1.length != arr2.length){
+            return false;
+        }else{
+            for(String c: arr1){
+                for(int i = 0; i < arr2.length; i++){
+                    if(c.equalsIgnoreCase(arr2[i])){
+                        count++;
+                        arr2[i] = null;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(count == arr2.length){
+            return true;
+        }
+
+                return false;
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
