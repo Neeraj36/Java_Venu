897a8a74f85659ae82a1c624312cc8dfcd818af1
Neeraj36 <53039469+Neeraj36@users.noreply.github.com>
2019-09-04 20:05:20 +0530

    Assignment - 6

---
+++ Car.java
@@ -0,0 +1,58 @@
+
+import java.io.*;
+
+@SuppressWarnings("serial")
+public class Car implements Serializable{
+    String VIN;
+    String make;
+    String model;
+    int year;
+    int mileage;
+    float price;
+    
+    Car() {};   
+    Car(String VIN, String make, String model, int year, int mileage, float price) {
+        this.VIN = VIN;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.price = price;
+    }
+    public boolean addNewCar(String VIN, String make, String model, int year, int mileage, float price) {
+        this.VIN = VIN;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.price = price;
+        return true;
+    }
+
+    public String getVIN() {
+        return VIN;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    
+    
+   
+}
